c949f68638ad6e8f291bd00058da3b6902cc38af
guptapalak2006 <guptapalak.735150@gmail.com>
2025-11-25 01:46:56 +0530

    Create MeetingRepository.java

---
+++ MeetingRepository.java
@@ -0,0 +1,9 @@
+package com.virtualclassroom.repository;
+
+import com.virtualclassroom.model.Meeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+}
